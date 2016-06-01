a51b3137008bf39de90727e2a91d420716e2afc1
Chiniwini12 <ilv22@hotmail.com>
2016-06-01 16:34:59 -0300

    Ciclos :8ball:
    Aqui etoy aprendiendo:P

---
+++ Ciclos/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Chinigüini
+ * Fecha: 01/06/2016
+ * Hora: 16:33
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Ciclos
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
